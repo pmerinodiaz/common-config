f033064cf046ec12352a41bba8dc8646c2eabebb
Patricio Merino <pmerinodiaz@gmail.com>
2021-08-30 20:38:37 -0400

    Fixes

--- local-install-mac.sh
+++ local-install-mac.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+username=${SUDO_USER:-$USER}
+export HOME_FOLDER="/home/$username"
+export FCH_CHILE_SDK_FOLDER="$HOME_FOLDER/fch-chile-sdk"
+FILE="$FCH_CHILE_SDK_FOLDER/common-config/vscode.deb"
 
-# 1. Install VSCode
-brew update
-brew tap caskroom/cask
-brew cask search visual-studio-code
-brew cask install visual-studio-code
+if [ ! -f "$FILE" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # 1. Install VSCode
+    brew update
+    brew tap caskroom/cask
+    brew cask search visual-studio-code
+    brew cask install visual-studio-code
+fi
--- local-install-ubuntu-debian.sh
+++ local-install-ubuntu-debian.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# 1. Install VSCode
-sudo apt update -y
-sudo apt install -y wget
-wget -O vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-sudo apt install -y ./vscode.deb
+username=${SUDO_USER:-$USER}
+export HOME_FOLDER="/home/$username"
+export FCH_CHILE_SDK_FOLDER="$HOME_FOLDER/fch-chile-sdk"
+FILE="$FCH_CHILE_SDK_FOLDER/common-config/vscode.deb"
+
+if [ ! -f "$FILE" ]; then
+    # 1. Install VSCode
+    sudo apt update -y
+    sudo apt install -y wget
+    wget -O vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+    sudo apt install -y ./vscode.deb
+fi
